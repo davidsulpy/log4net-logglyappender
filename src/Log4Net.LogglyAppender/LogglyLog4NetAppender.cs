7029dcd5b9f4f0c4b8b81df0837310b9bf261d89
David Sulpy <david@sulpy.com>
2013-12-21 16:25:21 -0600

    new loggly log4net appender to ship logs to loggly through async RestSharp calls over https

---
+++ src/Log4Net.LogglyAppender/LogglyLog4NetAppender.cs
@@ -0,0 +1,54 @@
+﻿using log4net.Appender;
+using log4net.Core;
+using RestSharp;
+using RestSharp.Serializers;
+
+namespace Log4Net.LogglyAppender
+{
+    public class LogglyLog4NetAppender : AppenderSkeleton
+    {
+        public string InputKey { get; set; }
+        public string LogUrl { get; set; }
+        public string Environment { get; set; }
+        public string Application { get; set; }
+        public string Role { get; set; }
+        public int TimeoutInSeconds { get; set; }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            string logMessage = RenderLoggingEvent(loggingEvent);
+
+            Send(logMessage);
+        }
+
+        public virtual void Send(string message)
+        {
+            string baseUrl = string.Format("{0}{1}/tag/{2},{3},{4}", LogUrl, InputKey, Application, Environment, Role);
+            var client = new RestClient(baseUrl);
+            client.Timeout = TimeoutInSeconds*1000;
+
+            var request = new RestRequest("", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.JsonSerializer = new JsonNonSerializer();
+            request.AddBody(message);
+
+            client.ExecuteAsync(request, response => { });
+        }
+
+        /// <summary>
+        ///     This is a trick to prevent RestSharp from trying to be nice and serialize when it doesn't have to
+        /// </summary>
+        private class JsonNonSerializer : ISerializer
+        {
+            public string Serialize(object obj)
+            {
+                return obj.ToString();
+            }
+
+            public string RootElement { get; set; }
+            public string Namespace { get; set; }
+            public string DateFormat { get; set; }
+            public string ContentType { get; set; }
+        }
+    }
+}
